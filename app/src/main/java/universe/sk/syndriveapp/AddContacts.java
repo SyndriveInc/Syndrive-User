5da941b8acdd6300c9a3084c49ad75545c62011d
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-08-10 13:47:45 +0530

    Intent

--- app/src/main/java/universe/sk/syndriveapp/AddContacts.java
+++ app/src/main/java/universe/sk/syndriveapp/AddContacts.java
@@ -1,15 +1,21 @@
 package universe.sk.syndriveapp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class AddContacts extends AppCompatActivity {
 
     FloatingActionButton fabAdd;
     Button btnRegister;
+    private final int REQUEST_CONTACTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,22 @@ public class AddContacts extends AppCompatActivity {
         fabAdd = findViewById(R.id.fabAdd);
         btnRegister = findViewById(R.id.btn_register);
 
-        
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DEFAULT, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, REQUEST_CONTACTS);
+            }
+        });
+    } // end of onCreate
 
-    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CONTACTS && resultCode == RESULT_OK) {
+            Uri contactData = data.getData();
+            
+        }
+    } // end of onActivityResult
 }
