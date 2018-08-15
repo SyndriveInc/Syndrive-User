8cdd6e64e9a75e24d8adba0c514e3f0a569cebae
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-08-15 12:11:48 +0530

    Contact Adapter

--- app/src/main/java/universe/sk/syndriveapp/ContactAdapter.java
+++ app/src/main/java/universe/sk/syndriveapp/ContactAdapter.java
@@ -1,4 +1,18 @@
 package universe.sk.syndriveapp;
 
-public class ContactAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactAdapter extends ArrayAdapter<Contact> {
+//    private final Context context;
+//    private final ArrayList<Contact> contacts;
+
+
+    public ContactAdapter(@NonNull Context context, int resource, @NonNull List<Contact> objects) {
+        super(context, resource, objects);
+    }
 }
